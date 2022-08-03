aca032f59cf4ecae70f3cb3459c63262c3e8c619
SamuelSilva15 <Silvamsamuel15@gmail.com>
2022-08-03 19:00:46 -0300

    Adicionando trajetos no mapa

--- App.js
+++ App.js
@@ -1,20 +1,21 @@
 import * as Location from 'expo-location';
 import * as Permissions from 'expo-permissions';
 import React, { useEffect, useRef, useState } from 'react';
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 import MapView from 'react-native-maps';
-
-
 import MapViewDirections from 'react-native-maps-directions';
 
 import { css } from './assets/css/Css';
 import config from './config/index.json';
 
+
 export default function App() {
 
+    const mapEl = useRef(null);
     const [origin, setOrigin]=useState(null);
     const [destination, setDestination]=useState(null);
+    const [distancia, setDistancia]=useState(null);
 
     useEffect(() => {
         (async function(){
@@ -39,7 +40,10 @@ export default function App() {
         style={css.map}
         initialRegion={origin}
         showsUserLocation={true}
-        loadingEnabled={true}>
+        zoomEnabled={true}
+        loadingEnabled={true}
+        ref={mapEl}
+        >
 
           {destination &&
           <MapViewDirections
@@ -48,34 +52,49 @@ export default function App() {
             apikey={config.googleApi}
             strokeWidt={3}
             onReady={result=>{
-              console.log(result);
-            }}/>
+              setDistancia(result.distance);
+              mapEl.current.fitToCoordinates(
+                result.coordinates, {
+                  edgePadding: {
+                    top: 100,
+                    bottom: 100,
+                    left: 100,
+                    right: 100
+                  }
+                }
+              )
+            }}
+            />
           }
-           
+          
         </MapView>
 
       <View style={css.search}>
         <GooglePlacesAutocomplete
-        placeholder='Para onde vamos?'
-        onPress={(data, details = null) => {
-          // 'details' is provided when fetchDetails = true
-          setDestination({
-            latitude: details.geometry.latitude,
-            longitude: details.geometry.longitude,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
-          });
-          console.log(destination);
-        }}
-        query={{
-          key: config.googleApi,
-          language: 'pt-br',
-        }}
-        enablePoweredByContainer={false}
-        fetchDetails={true}
-        styles={{listView:{height: 100}}}
-    />
+          placeholder='Para onde vamos?'
+          onPress={(data, details = null) => {
+            setDestination({
+              latitude: details.geometry.location.lat,
+              longitude: details.geometry.location.lng,
+              latitudeDelta: 0.000922,
+              longitudeDelta: 0.000421,
+            });
+            console.log(destination);
+          }}
+          query={{
+            key: config.googleApi,
+            language: 'pt-br',
+          }}
+          enablePoweredByContainer={false}
+          fetchDetails={true}
+          styles={{listView:{height: 100}}}
+        />
+        <View>
+        {distancia &&
+          <Text>Distância: {distancia}m</Text>
+        }
       </View>
-      </View>
+    </View>
+    </View>
     );
 }
